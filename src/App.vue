02f6f477e106c35e725b6c7c318ddcb500f0d892
2547technowizz <neil_edward_lobo@hotmail.com>
2021-09-03 20:48:40 -0400

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
+    <Navbar />
+    <router-view></router-view>
   </div>
 </template>
 
+<script>
+import Navbar from "@/components/Navbar.vue"
+
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+}
+</script>
+
+
 <style lang="scss">
+// Global styles
+</style>
+
+<style lang="scss" scoped>
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-app-bar
+        app
+        flat
+        color="#1A1D38"
+        class="nav-bar"
+    >
+    <div>
+        <v-btn text class="nav-button" to="/vods" color=white active-class="active-nav">
+            <span class="text">VODS</span>
+        </v-btn>
+        <v-btn text class="nav-button" to="/chats" color=white active-class="active-nav">
+            <span class="text">CHATS</span>
+        </v-btn>
+    </div>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+.nav-button {
+    margin: 0 15px 0 15px;
+
+    .text {
+        margin: 0 7px 0 7px;
+        font-family: 'Roboto', sans-serif;
+        text-align: center;
+        font-weight: 300;
+        color: white;
+    }
+}
+
+.active-nav {
+    background-color: rgba(255, 255, 255, 0.25);
+}
+
+.join-button {
+    .text {
+        color: white;
+    }
+}
+</style>
