5b7d707cdefb2dd50d42d8917cadac4bd3e1e669
2547technowizz <neil_edward_lobo@hotmail.com>
2021-09-09 22:08:29 -0400

    readded history mode and added middleware to fixed refresh issue with history mode

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express")
 const cors = require("cors");
+const history = require("connect-history-api-fallback")
 const app = express()
+
+app.use(history())
 app.use(express.static(`dist/`))
 app.use(cors())
 
-app.listen(process.env.PORT || 80, () => {
-    console.log(`Listening on port: ${process.env.PORT || 80}`)
+function log(str) {
+    const date = new Date()
+    console.log(`(UTC/GMT${(-1)*date.getTimezoneOffset()/60}) ${date.toLocaleString()} |> ${str}`)
+}
+
+app.get("/", (req, res) => {
+    res.sendFile(`index.html`)
 })
 
-app.get("/*", (req, res) => {
-    res.sendFile(`index.html`)
-})  
+app.listen(process.env.PORT || 5000, () => {
+    log(`Listening on port: ${process.env.PORT || 5000}`)
+})
